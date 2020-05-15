9b3415e8a7fac50571756631e1bb14b0a072678f
Michelle <michedomingo@gmail.com>
2020-05-15 09:18:13 +0000

    Work in progress

--- main.c
+++ main.c
@@ -6,23 +6,72 @@
  * @argv: command line arguments (av[1] should be filename)
  * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
+	FILE *monty_file;
+	char *line = NULL;
+	char **commands;
+	size_t len = 0;
+	ssize_t nread;
+	int line_number = 0;
+
+
 	/* only one arg to program allowed */
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	monty_file = fopen(argv[1], "r");
-	if(monty_file == -1)
+	if (!monty_file)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	/* WIP: trying to figure out operation code parsing  */
+	/* getline returns #chars read (excluding '\0') */
+	while ((nread = getline(&line, &len, monty_file)) != -1)
+	{
+		printf("Retrieved line of length %zu:\n", nread);
+		line_number++;
+		commands = get_tokens(line);
+		printf("line number: %d\n", line_number);
+		printf("token1: line[0] %s\n", tokens[0]);
+		printf("token2: line[1] %s\n", tokens[1]);
+	}
 
+
+	free(line);
+	fclose(monty_file);
 	return (0);
+}
+
+/**
+ * get_tokens -
+ * @input:
+ * Return:
+ */
+char **get_tokens(char *input)
+{
+	int i;
+	char *str_tok;
+	char **tokens;
+
+	tokens = malloc(sizeof(size_t);
+	if (!tokens)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	str_tok = strtok(input, DELIM);
+
+	i = 0;
+	while (str_tok)
+	{
+		tokens[i] = str_tok;
+		i++;
+		str_tok = strtok(NULL, DELIM);
+	}
+	return (tokens);
 }
--- monty.h
+++ monty.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define DELIM "\n\r "
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -16,9 +18,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,8 +33,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 
